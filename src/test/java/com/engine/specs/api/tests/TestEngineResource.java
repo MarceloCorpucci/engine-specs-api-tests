0c52791edcb8e5cde4b5cfdf4905a823543db700
Marcelo Corpucci <mcorpucci@gmail.com>
2020-02-25 09:03:38 -0300

    Added test case, builder implemented. Pending to define optional values.

--- src/test/java/com/engine/specs/api/tests/TestEngineResource.java
+++ src/test/java/com/engine/specs/api/tests/TestEngineResource.java
@@ -1,5 +1,47 @@
 package com.engine.specs.api.tests;
 
-public class TestEngineResource {
+import static io.restassured.RestAssured.delete;
+import static io.restassured.RestAssured.given;
 
+import com.engine.specs.api.entity.Engine;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestEngineResource {
+	private final String endPoint = "http://localhost:5000/api";
+	private Engine engine;
+	
+	@Before
+	public void setUp() {
+		engine = new Engine.Builder()
+								.model("L61")
+								.displacement(2200)
+								.power(147)
+								.forcedInduction(false)
+								.build();
+	}
+	
+	@Test
+	public void engineCreatedSuccessfuly() {
+		given()
+			.contentType("application/json")
+			.body(engine)
+			.log()
+			.all()
+		.when()
+			.post(endPoint + "/engine")
+		.then()
+			.assertThat()
+			.statusCode(201)
+			.log()
+			.all();
+	}
+	
+	@After
+	public void tearDown() {
+		String engineId = "";
+		//delete(endPoint + "/engine/" + engineId);
+	}
 }
