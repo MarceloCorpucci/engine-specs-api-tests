6ad052b1788255d419331530fc98a8f97e6c90b0
Marcelo Corpucci <mcorpucci@gmail.com>
2020-02-27 08:36:21 -0300

    Added a new test for engine resource.

--- src/test/java/com/engine/specs/api/tests/TestEngineResource.java
+++ src/test/java/com/engine/specs/api/tests/TestEngineResource.java
@@ -6,6 +6,9 @@ import static io.restassured.RestAssured.given;
 import com.engine.specs.api.entity.Engine;
 
 import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.After;
 import org.junit.Before;
@@ -14,6 +17,7 @@ import org.junit.Test;
 public class TestEngineResource {
 	private final String endPoint = "http://localhost:5000/api";
 	private Engine engine;
+	private RequestSpecification request;
 	
 	@Before
 	public void setUp() {
@@ -23,22 +27,42 @@ public class TestEngineResource {
 								.power(147)
 								.forcedInduction(false)
 								.build();
+		
+		request = given()
+					.contentType("application/json")
+					.body(engine)
+					.log()
+					.all();
 	}
 	
 	@Test
-	public void engineCreatedSuccessfuly() {
-		given()
-			.contentType("application/json")
-			.body(engine)
-			.log()
-			.all()
-		.when()
-			.post(endPoint + "/engine")
-		.then()
-			.assertThat()
-			.statusCode(201)
-			.log()
-			.all();
+	public void engineCreatedShouldHaveProperStatusCode() {
+		request
+			.when()
+				.post(endPoint + "/engine")
+			.then()
+				.assertThat()
+				.statusCode(201)
+				.log()
+				.all();
+	}
+	
+	@Test
+	public void engineCreatedShouldShowItsContent() {
+		request
+			.when()
+				.post(endPoint + "/engine")
+			.then()
+				.assertThat()
+				.body("engine.displacement", equalTo(engine.getdisplacement()))
+				.and()
+				.body("engine.forced_induction", equalTo(engine.isforced_induction()))
+				.and()
+				.body("engine.model", equalTo(engine.getmodel()))
+				.and()
+				.body("engine.power", equalTo(engine.getpower()))
+				.and()
+				.body("engine.valve_amount", equalTo(engine.getvalve_amount()));
 	}
 	
 	@After
