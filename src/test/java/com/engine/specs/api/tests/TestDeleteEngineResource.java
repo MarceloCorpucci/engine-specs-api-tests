5cdf52582110fcdc91423e69def1ff37b29e1564
Marcelo Corpucci <mcorpucci@gmail.com>
2020-02-28 08:26:28 -0300

    Added scenario to delete engine.

---
+++ src/test/java/com/engine/specs/api/tests/TestDeleteEngineResource.java
@@ -0,0 +1,45 @@
+package com.engine.specs.api.tests;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.engine.specs.api.entity.Engine;
+
+import io.restassured.RestAssured;
+
+public class TestDeleteEngineResource {
+	private final String endPoint = "http://localhost:5000/api";
+	private Engine engine;
+	private String engineId;
+	
+	@Before
+	public void setUp() {
+		engine = new Engine.Builder()
+								.model("L61")
+								.displacement(2200)
+								.power(147)
+								.forcedInduction(false)
+								.build();
+		
+		engineId = given()
+					.contentType("application/json")
+					.body(engine)
+					.post(endPoint + "/engine")
+					.getBody()
+					.jsonPath()
+					.getString(
+							String.format("%s", "engine._id.$oid"));
+	}
+	
+	@Test
+	public void engineDeletedShouldHaveProperStatusCode() {
+			RestAssured
+				.when()
+					.delete(endPoint + "/engine/" + engineId)
+				.then()
+					.assertThat()
+					.statusCode(204);
+	}
+}
